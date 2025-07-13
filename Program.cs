0d4aa1d89de0eb917c625e709facbce8b0e8155b
Amani Khalid <amanikhalidd@gmail.com>
2025-07-13 09:11:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleHotelManagementSystem_OOPTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
