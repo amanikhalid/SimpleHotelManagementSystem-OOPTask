8fbca1253c3407fbc8272601c568e5aa07ddff76
Amani Khalid <amanikhalidd@gmail.com>
2025-07-13 11:38:32 +0400

    Added A static method that prints a welcome message to the guest

---
+++ HotelUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelManagementSystem_OOPTask
+{
+    class HotelUtils
+    {
+        // A static method PrintWelcomeMessage() that prints a welcome message to the guest
+        public static void PrintWelcomeMessage(string guestName)
+        {
+            if (string.IsNullOrEmpty(guestName))
+            {
+                throw new ArgumentException("Guest name cannot be null or empty.");
+            }
+            Console.WriteLine($"Welcome to our hotel, {guestName}! We hope you have a pleasant stay.");
+        }
+    }
+}
