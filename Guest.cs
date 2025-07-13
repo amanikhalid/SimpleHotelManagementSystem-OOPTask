2881bcf0335759f0322e4f28f8bb24113de9b1d8
Amani Khalid <amanikhalidd@gmail.com>
2025-07-13 10:55:28 +0400

    Added public string NationalID get: set/ Added validation

--- Guest.cs
+++ Guest.cs
@@ -36,7 +36,24 @@ namespace SimpleHotelManagementSystem_OOPTask
 
 
 //Auto-property: NationalID
-public string NationalID { get; set; }
+public string NationalID
+        { 
+            get { return NationalID; }
+            set
+            {
+                // Validation for NationalID property
+                if (string.IsNullOrWhiteSpace(value) || value.Length != 6)
+                {
+                    Console.WriteLine("National ID must be exactly 6 characters long and cannot be empty or whitespace.");
+                }
+                else
+                {
+                    NationalID = value;
+                }
+            }
+
+
+        }
 
         // Constructor overloads to Guest class 
         public Guest()
